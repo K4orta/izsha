0abe089c895fbdea07e6c8d2fbaf6518abe12b09
Erik Wong <k4orta@gmail.com>
2013-07-15 12:05:38 -0700

    Added a delay between getting a file event and parsing

--- izsha/replay_event.py
+++ izsha/replay_event.py
@@ -1,13 +1,18 @@
-import os
+import os, re, time
 from watchdog.events import FileSystemEventHandler
 
 class OnReplay(FileSystemEventHandler):
     def __init__(self, process_callback, on_finish):
         self.__process_callback = process_callback
         self.__on_finish = on_finish
+        self.__replay_ext_re = re.compile("(.SC2Replay$)", re.I)
 
     def on_modified(self, event):
-        self.__on_finish(self.__process_callback(event.src_path))
+        if os.path.isfile(event.src_path) and self.__replay_ext_re.search(event.src_path):
+            time.sleep(1)
+            self.__on_finish(self.__process_callback(event.src_path))
 
     def on_created(self, event):
-        self.__on_finish(self.__process_callback(event.src_path))
+        if os.path.isfile(event.src_path) and self.__replay_ext_re.search(event.src_path):
+            time.sleep(1)
+            self.__on_finish(self.__process_callback(event.src_path))
